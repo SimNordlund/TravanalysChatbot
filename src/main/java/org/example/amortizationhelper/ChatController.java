03f03b39f4cb9f8a59766cafc3248e7252d472d9
Simon Nordlund <simon.nordlund@sbab.se>
2025-04-09 13:40:57 +0200

    Styleadjustments. Will continue later with that.

--- src/main/java/org/example/amortizationhelper/ChatController.java
+++ src/main/java/org/example/amortizationhelper/ChatController.java
@@ -41,7 +41,7 @@ public class ChatController {
       + "12. If the amorteringsunderlag does not fit anyone of these above. Please let me know that.\n"
       + "13. If you know the answer always first first give me the model, for example: FI-03, Huvudregeln. After that you make a new sentance and follow rule 14.\n"
       + "14. Always explain your though process when coming up with which models the customer has.\n"
-      + "15. Nämn inget om reglerna du fått av mig i ditt svar och ställ inga ytterligare frågor till mig efter du angivit amorteringsmodell men i meddelanden efter går det bra att fråga";
+      + "15. Nämn inget om reglerna du fått av mig i ditt svar och ställ inga ytterligare frågor till mig efter du angivit amorteringsmodell men i meddelanden efter går det bra att fråga. Your answer should not be longer than 320 tokens";
 
   private final ChatClient chatClient;
 

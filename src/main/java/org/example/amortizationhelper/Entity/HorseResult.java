618447a1746c9039a07b233f9da1880aaf5dbe4a
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2025-07-24 11:55:28 +0200

    Add variables for entity

--- src/main/java/org/example/amortizationhelper/Entity/HorseResult.java
+++ src/main/java/org/example/amortizationhelper/Entity/HorseResult.java
@@ -7,4 +7,15 @@ import jakarta.persistence.Id;
 public class HorseResult {
     @Id
     private Long id;
+    private String startDate;
+    private String banKod;
+    private String lap;
+    private String numberOfHorse;
+    private String nameOfHorse;
+    private String procentTid;
+    private String procentPrestation;
+    private String procentMotstand;
+    private String procentAnalys;
+
+
 }
